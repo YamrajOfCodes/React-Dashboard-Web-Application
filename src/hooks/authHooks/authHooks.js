993aeb3ccd45168892d016609aaa6d127a634d40
YamrajOfcodes <kundanpatil0111@gmail.com>
2026-04-28 13:56:58 +0530

    Prevents  the Login System,  After login,  Page is not found came

--- src/hooks/authHooks/authHooks.js
+++ src/hooks/authHooks/authHooks.js
@@ -1,23 +1,23 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { login, logout, register } from "../../types/Auth/authAPI"
-
-import toast from 'react-hot-toast';
-import {jwtDecode} from 'jwt-decode';
-import { AxiosError } from 'axios';
-
+import { useMutation } from "@tanstack/react-query";
+import { login, logout, register } from "../../types/Auth/authAPI";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 export const useLogin = () => {
+  const navigate = useNavigate();
+
   return useMutation({
     mutationFn: login,
 
     onSuccess: (data) => {
       localStorage.setItem("token", data.token);
-
       localStorage.setItem("user", JSON.stringify(data));
-      toast.success("login successfull")
+
+      toast.success("Login successful");
+
       setTimeout(() => {
-        window.location.href = "/dashboard";
-      }, 500);
+        navigate("/dashboard"); 
+      }, 300);
     },
 
     onError: () => {
@@ -27,16 +27,18 @@ export const useLogin = () => {
 };
 
 export const useLogout = () => {
-  const logout = () => {
-    localStorage.removeItem("token");
-    localStorage.removeItem("user");;
+  const navigate = useNavigate();
 
-    toast.success("logout successfull");
+  const logoutUser = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+
+    toast.success("Logout successful");
 
     setTimeout(() => {
-      window.location.href = "/";
-    }, 500);
+      navigate("/"); 
+    }, 300);
   };
 
-  return { logout };
+  return { logout: logoutUser };
 };
